3a0f2c425571c6abe376bb2e8883b2f8272bd3d8
chemaeva8898@gmail.com <chemaeva8898@gmail.com>
2021-11-25 19:47:12 +1000

    Добавьте файлы проекта.

---
+++ lab4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N = Convert.ToDouble(Console.ReadLine());
+
+            double maxnumber = 2 * N - 1;
+            double number = 2;
+            double answer = 0;
+            for (int i = 0; i < maxnumber / 2; i++)
+            {
+                if (maxnumber > number)
+                {
+                    answer += maxnumber - number;
+                    number += 2;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("Квадрат числа N = " + Math.Sqrt(answer));
+                Console.ReadLine();
+            
+        }
+    }
+}
